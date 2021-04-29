7c8838072f934079e5414aea19edea033e528e17
jacobrdavis <jacobdavis4085@gmail.com>
2021-04-29 11:48:25 -0400

    fft updates

--- ORRE_post_processing.m
+++ ORRE_post_processing.m
@@ -1,23 +1,20 @@
 %% ------------------------------ Header ------------------------------- %%
 % Filename:     ORRE_post_processing.m
-% Description:  ORRE Post Processing Program input file (test)
+% Description:  ORRE Post Processing Program run file
 % Authors:      D. Lukas and J. Davis
 % Created on:   6-10-20
 % Last updated: 8-9-20 by J. Davis
-%% --------------------------- Settings -------------------------------- %%
-
-close all % close any open figures
-
-% Execute app:
-
-run_app = 0;
+%% ------------------------------ Inputs ------------------------------- %%
+run_app = 1;
 show_examples = 0;
+UserDefinedScript = 'OSWEC_inertia.m';
 
-if run_app == 1
-    pkg.ORRE_post_processing_app
+%% -------------------------------- Run -------------------------------- %%
+if run_app == 1 
+    ORRE_post_processing_app    % Execute app
+else  
+    run(UserDefinedScript)          % Run script
 end
-
-
 %% ----------------------------- Examples ------------------------------ %%
 if show_examples == 1
     
@@ -85,118 +82,6 @@ xlabel(data.map(strcat('ch',n)))
 ylabel(data.map(strcat('ch',m)))
 
 end
-
-%% ----------------------------- Scripting ----------------------------- %%
-if run_app ~= 1
-     
-% Define inputs:
-directory = "C:\Users\orre2\Desktop\OSWEC_inertia\";     % current directory
-filename = "run1.txt";
-datatype = 1;
-ntaglines = 12;
-nheaderlines = 1;
-tagformat = '%s';
-headerformat = '%s';
-dataformat =  '%f';
-headerdelimiter = char(9); %tab
-datadelimiter = ' ';
-commentstyle = '%';
-
-data.run1 = pkg.fun.read_data(directory,filename,datatype,ntaglines,...
-    nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
-
-data.run2 = pkg.fun.read_data(directory,filename,datatype,ntaglines,...
-    nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
-
-data.run3 = pkg.fun.read_data(directory,filename,datatype,ntaglines,...
-    nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
-
-
-end
-
-
-% 
-%   for k = 1:length(myFiles)
-%         baseFileName = myFiles(k).name;
-%         fullFileName = fullfile(myDir, baseFileName);  % Changed myFolder to myDir
-%         fprintf(1, 'Now reading %s\n', fullFileName);
-%         data = pkg.fun.read_data(myDir,baseFileName,datatype);
-%         data.ch1 = sqrt(data.ch1).*Ts;
-%         dataset{k,1} = data;
-%         if writedata == 1
-%             combined_data = [];
-%             for ch = 1:length(data.headers)
-%                 combined_data(:,ch) = data.(strcat('ch',num2str(ch)));
-%             end
-%             
-%             data.tags = split(data.tags{1},char(9));
-%             datestring = split(data.tags{2},'_');
-%             Y = extractBetween(datestring{2},1,4);
-%             M = extractBetween(datestring{2},5,6);
-%             D = extractBetween(datestring{2},7,8);
-%             
-%             data.tags{2} = datestr(datetime(cellfun(@str2num,{Y{1} M{1} D{1}})));
-%             
-%             outputdir = 'C:\Users\jacob\Documents\ORRE_Offline\NREL TCF\OSWEC\W2 Testing\Fixed\';
-%             headerfmt = [repmat('%s,',1,length(data.headers)-1),'%s'];
-%             
-%             fid = fopen(strcat(outputdir,data.filename),'wt'); 
-%             fprintf(fid, '%s %s %s \n', data.tags{1}, data.tags{3}, data.tags{2});
-%             fprintf(fid, headerfmt, data.headers{:});
-%             fclose(fid);
-%             
-% %           dlmwrite(strcat(outputdir,data.filename),combined_data,'delimiter','\t','-append','precision','%10.8f')           
-%             writematrix(combined_data,strcat(outputdir,data.filename),'WriteMode','append')      
-%         end
-%     end
-
-%     writedata = 0;
-%     
-%     Ts = 0.020000;
-%     
-%     datatype = 1;
-%     
-%     myDir = 'C:\Users\jacob\Documents\ORRE_Offline\NREL TCF\OSWEC\W2 Testing\Regular Wave Response\0 Deg\';
-%     myFiles = dir(fullfile(myDir,'*.txt'));
-%    
-%     dataset = cell(length(myFiles),1);
-%     
-%     for k = 1:length(myFiles)
-%         baseFileName = myFiles(k).name;
-%         fullFileName = fullfile(myDir, baseFileName);  % Changed myFolder to myDir
-%         fprintf(1, 'Now reading %s\n', fullFileName);
-%         data = pkg.fun.read_data(myDir,baseFileName,datatype,0,1,'~','~','~',' ','~');
-%         dataset{k,1} = data;
-%         
-%     end
-%     
-%     
-%     myDir2 = 'C:\Users\jacob\Documents\ORRE_Offline\NREL TCF\OSWEC\W2 Testing\From Mike\WEC testing\Labview DATA\Waves 0Deg\Test\';
-%     myFiles2 = dir(fullfile(myDir2,'*.txt'));
-%    
-%     dataset2 = cell(length(myFiles2),1);
-%     
-%     for k = 1:length(myFiles2)
-%         baseFileName2 = myFiles2(k).name;
-%         fullFileName2 = fullfile(myDir2, baseFileName2);  % Changed myFolder to myDir
-%         fprintf(1, 'Now reading %s\n', fullFileName2);
-%         data2 = pkg.fun.read_data(myDir2,baseFileName2,datatype);
-%         data2.ch1 = sqrt(data2.ch1).*Ts;
-%         dataset2{k,1} = data2;
-%     end
-%     
-%     plot(dataset{14}.ch1,dataset{14}.ch2)
-%     
-%     plot(dataset2{14}.ch1,dataset2{14}.ch7); hold on
-%     plot(dataset2{14}.ch1,dataset2{14}.ch8);
-%     
-%     h = 0.631/2;
-%     asin((dataset2{14}.ch7 - mean(dataset2{14}.ch7))/h)*180/pi;
-%     
-%      plot(dataset2{14}.ch1,dataset2{14}.ch7 - dataset2{14}.ch8)
-%      
-%      response = asin(abs(dataset2{14}.ch7)/h);
-
 
 
 
--- OSWEC_inertia.m
+++ OSWEC_inertia.m
@@ -4,12 +4,12 @@
 % Authors:      D. Lukas and J. Davis
 % Created on:   6-10-20
 % Last updated: 8-9-20 by J. Davis
-%% --------------------------- Settings -------------------------------- %%
+%% ------------------------------------ -------------------------------- %%
 
 close all % close any open figures
 
 % Define inputs:
-directory = "C:\Users\orre2\Desktop\OSWEC_inertia\";     % current directory
+directory = "data\OSWEC_inertia\";     % current directory
 filename = "run1.txt";
 datatype = 1;
 ntaglines = 12;
@@ -21,16 +21,28 @@ headerdelimiter = char(9); %tab
 datadelimiter = ' ';
 commentstyle = '%';
 
-data.run1 = pkg.fun.read_data(directory,"run1.txt",datatype,ntaglines,...
-    nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
+% Read in data if it is not already in the workspace
+if ~exist('data','var')
+    data.run1 = pkg.fun.read_data(directory,"run1.txt",datatype,ntaglines,...
+        nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
 
-data.run2 = pkg.fun.read_data(directory,"run2.txt",datatype,ntaglines,...
-    nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
+    data.run2 = pkg.fun.read_data(directory,"run2.txt",datatype,ntaglines,...
+        nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
 
-data.run3 = pkg.fun.read_data(directory,"run3.txt",datatype,ntaglines,...
-    nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
+    data.run3 = pkg.fun.read_data(directory,"run3.txt",datatype,ntaglines,...
+        nheaderlines,tagformat,headerformat,dataformat,headerdelimiter,datadelimiter,commentstyle);
+end
 
-dominant_periods = zeros(3,1);
+
+%%%%% moment of inertia calc
+
+g = 9.81;
+d = 0.2659; % from SW, no ballast
+M = 6.3037; % from SW, no ballast
+
+Tn  = zeros(3,1); % initialize vector to store dominant periods
+wn  = zeros(3,1); 
+Iyy = zeros(3,1);
 
 for num = 1:3
     run = ['run',num2str(num)];
@@ -40,15 +52,19 @@ for num = 1:3
     phi_raw  = data.(run).ch2(data.(run).ch1 >= t0); phi_raw = phi_raw-mean(phi_raw(end-100:end));
 
     % filter
+    [f_raw,P_raw] = pkg.fun.fft(t,phi_raw);
+    %data.run1.addprop('fft')
+    %data.run1.fft.fs = (2*10^(-3))^(-1);
+    %pkg.fun.fft(1,2,data.run1);
     %phi_f = bandpass(phi,[.8,1.2],(2*10^-3)^(-1));
-    [phi,filter] = lowpass(phi_raw,1,(2*10^-3)^(-1),'Steepness',0.95);
-plot(t,phi)
-    [f1,P1,dominant_periods(num)] = pkg.fun.plt_fft(t,phi_raw); hold on
-    [f2,P2] = pkg.fun.plt_fft(t,phi);
-
+    passbandfreq = 2; % Hz
+    [phi,filter] = lowpass(phi_raw,passbandfreq,(2*10^-3)^(-1),'Steepness',0.95);
+    [f,P,Tn(num)] = pkg.fun.fft(t,phi);
+    
     figure
-        semilogx(f1,P1,'DisplayName','Original'); hold on
-        semilogx(f2,P2,'DisplayName','Filtered'); 
+        semilogx(f_raw,P_raw,'DisplayName','Original'); hold on
+        semilogx(f,P,'DisplayName','Filtered'); 
+        xline(passbandfreq,'DisplayName','Passband frequency')
         legend()
         xlabel('f(Hz)')
         ylabel('P1')
@@ -59,7 +75,26 @@ plot(t,phi)
         legend()
         xlabel('t(s)')
         ylabel('phi(deg)')
+        
+wn(num)  = 2*pi/Tn(num);
+Iyy(num) = M*g*d/wn(num)^2; % kg-m^2        
+
 end
 
-Tn_mean = mean(dominant_periods)
-Tn_stdev = std(dominant_periods)
+Tn_mean  = mean(Tn);
+Tn_stdev = std(Tn);
+
+Iyy_mean = mean(Iyy);
+Iyy_std  = std(Iyy);
+
+IyySW   = 0.6583; % kg-m^2
+
+figure
+bar(1,Iyy_mean,'DisplayName','Swing Test'); hold on
+    er = errorbar(1,Iyy_mean,Iyy_std,Iyy_std,'DisplayName','Swing Test STDEV');
+    er.Color = [0 0 0];                            
+    er.LineStyle = 'none';
+bar(2,IyySW,'DisplayName','SW')
+ylabel('Iyy (kg-m^2)')
+%xticks('exp','SW')
+legend()
